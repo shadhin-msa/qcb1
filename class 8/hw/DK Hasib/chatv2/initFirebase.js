97b0ef3ad0b46b3d7d5a11a1351e00fe55e54ddc
diia1000837hasib <mdhasib522@gmail.com>
2020-09-24 14:10:17 +0600

    dk- comlete hw

---
+++ class 8/hw/DK Hasib/chatv2/initFirebase.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+
+    apiKey: "AIzaSyB9KHz0nxh_WN-3Lph5UAw663goBNZ1HXw",
+    authDomain: "test-project-21e3f.firebaseapp.com",
+    databaseURL: "https://test-project-21e3f.firebaseio.com",
+    projectId: "test-project-21e3f",
+    storageBucket: "test-project-21e3f.appspot.com",
+    messagingSenderId: "198125223792",
+    appId: "1:198125223792:web:66e68fbf258810a97a59b5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+
