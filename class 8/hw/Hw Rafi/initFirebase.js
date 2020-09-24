8043e46b3ab4f19fd7a91e0e87b2ac1a94b739e3
AlRafi19 <aalzaman19@gmail.com>
2020-09-24 13:35:44 +0600

    firebase chat hw

--- class 8/hw/Hw Rafi/initFirebase.js
+++ class 8/hw/Hw Rafi/initFirebase.js
@@ -2,23 +2,17 @@
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     // rafi firebase log 
-    // apiKey: "AIzaSyD429JAfsxFCcuXuc4pz3ZJLSLEgaYFshI",
-    // authDomain: "fir-chat-453e9.firebaseapp.com",
-    // databaseURL: "https://fir-chat-453e9.firebaseio.com",
-    // projectId: "fir-chat-453e9",
-    // storageBucket: "fir-chat-453e9.appspot.com",
-    // messagingSenderId: "869254833780",
-    // appId: "1:869254833780:web:872ca9a70505e058cd1321",
-    // measurementId: "G-8YTKGS6C2B"
+    apiKey: "AIzaSyD429JAfsxFCcuXuc4pz3ZJLSLEgaYFshI",
+    authDomain: "fir-chat-453e9.firebaseapp.com",
+    databaseURL: "https://fir-chat-453e9.firebaseio.com",
+    projectId: "fir-chat-453e9",
+    storageBucket: "fir-chat-453e9.appspot.com",
+    messagingSenderId: "869254833780",
+    appId: "1:869254833780:web:872ca9a70505e058cd1321",
+    measurementId: "G-8YTKGS6C2B"
 
 
-    apiKey: "AIzaSyB9KHz0nxh_WN-3Lph5UAw663goBNZ1HXw",
-    authDomain: "test-project-21e3f.firebaseapp.com",
-    databaseURL: "https://test-project-21e3f.firebaseio.com",
-    projectId: "test-project-21e3f",
-    storageBucket: "test-project-21e3f.appspot.com",
-    messagingSenderId: "198125223792",
-    appId: "1:198125223792:web:66e68fbf258810a97a59b5"
+    
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
